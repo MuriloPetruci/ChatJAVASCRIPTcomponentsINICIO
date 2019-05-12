44c18fec5042e095eccfc9f7b271b14a32123534
MuriloPetruci <murilopetruci@hotmail.com>
2019-05-12 18:52:45 -0300

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/c4d44689-8d29-4d53-afbe-3af2f7c6352c/token";
+const instanceLocator = "v1:us1:c4d44689-8d29-4d53-afbe-3af2f7c6352c";
+
+export { tokenUrl, instanceLocator }
+
